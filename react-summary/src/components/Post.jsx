740725af8d63d8446a254b3707ca30470cde07b0
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-01-18 23:01:33 +0200

    Create summary project

---
+++ react-summary/src/components/Post.jsx
@@ -0,0 +1,8 @@
+function Post() {
+    return <div>
+        <p>Emanuel</p>
+        <p>I am starting to learn React</p>
+    </div>
+}
+
+export default Post;
---
+++ react-summary/src/main.jsx
@@ -0,0 +1,12 @@
+// this code is first executed in the browser
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
